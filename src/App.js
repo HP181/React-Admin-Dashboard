58785e979fa7ae1cfff34192b393027a8dae4653
Hitkumar <hit98987@gmail.com>
2022-11-22 13:32:30 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./App.css"
+
+const App = () => {
+  return (
+    <div className='bg-red-500 font-bold text-cyan-300 text-center h-32 flex items-center justify-center underline'>App</div>
+  )
+}
+
+export default App
